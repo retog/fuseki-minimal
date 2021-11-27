24960a4ccae74e14496e2cd6d9615c64ada94c70
Reto Gmür <reto@factsmission.com>
2021-11-27 00:09:41 +0000

    mini,al fuseki

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,14 +1,20 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
 package com.mycompany.app;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Remote World!" );
+import org.apache.jena.fuseki.main.FusekiServer;
+import org.apache.jena.fuseki.system.FusekiLogging;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.tdb.TDBFactory;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        FusekiLogging.setLogging();
+        String directory = "./dataset" ;
+        Dataset ds  = TDBFactory.createDataset(directory) ;
+        FusekiServer server = FusekiServer.create()
+                .add("/ds", ds)
+                .build();
+        server.start();
+        Thread.sleep(5 * 60 * 1000);
+        server.stop();
     }
 }
